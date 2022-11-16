891f48cbb24d8b7438817f2d01981c5eac073f6c
Rudie Dirkx <github@hotblocks.nl>
2022-11-16 15:46:38 +0100

    allow explicit NEW issue view

--- jiraredirect.sw.js
+++ jiraredirect.sw.js
@@ -1,6 +1,6 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
 	if (details.transitionType == 'link') {
-		if (details.url.includes('/browse/') && !details.url.match(/[?&]oldIssueView=/)) {
+		if (details.url.includes('/browse/') && !details.url.match(/[?&](old|new)IssueView=true/)) {
 			chrome.tabs.update(details.tabId, {
 				url: details.url,
 			});
