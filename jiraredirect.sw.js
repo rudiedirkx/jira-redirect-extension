3d285436d6f5543cc6c7ab5d4fa375be3dd6c3c2
Rudie Dirkx <github@hotblocks.nl>
2024-03-26 15:40:12 +0100

    redirect+

--- jiraredirect.sw.js
+++ jiraredirect.sw.js
@@ -1,6 +1,8 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
 	if (details.transitionType == 'link') {
-		if (details.url.includes('/browse/') && !details.url.match(/[?&](old|new)IssueView=true/)) {
+		const m1 = details.url.match(/\/(?:browse|issues)\/(\w+-\d+)/);
+		const m2 = details.url.match(/\/browse\/(\w+-\d+)$/);
+		if (m1 && !m2) {
 			chrome.tabs.update(details.tabId, {
 				url: details.url,
 			});
